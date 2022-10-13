56018ac105c4bf48a61f8e108f64f0659909230d
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-10-13 18:24:25 +0900

    Add unit tests for services.py

--- backend/ticketmgr/otphandler/tests.py
+++ backend/ticketmgr/otphandler/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from otphandler import services
 
-# Create your tests here.
+class ServicesTestCase(TestCase):
+    def test_totp_generation(self):
+        test_secret = b'DEBUGSECRETKEY2345=='
+        test_secret2 = b'SECRETDEBUGKEY2345=='
+        custom_totp = services.generate_totp(test_secret, True)
+        custom_totp2 = services.generate_totp(test_secret2, True)
+        pyhmac_totp = services.generate_totp(test_secret)
+
+        # Check correct HMAC computation of custom implementation
+        assert(custom_totp == pyhmac_totp)
+        # Check if different TOTP codes are generated for different secrets
+        assert(custom_totp != custom_totp2)
