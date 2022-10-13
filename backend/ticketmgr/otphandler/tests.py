5cf48acbca54b5576515ede5ce130e6c93f86b9f
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-10-13 18:40:10 +0900

    Add few more test cases for services.py

--- backend/ticketmgr/otphandler/tests.py
+++ backend/ticketmgr/otphandler/tests.py
@@ -9,7 +9,12 @@ class ServicesTestCase(TestCase):
         custom_totp2 = services.generate_totp(test_secret2, True)
         pyhmac_totp = services.generate_totp(test_secret)
 
+        # Check sanity check behavior of custom HMAC algorithm
+        with self.assertRaises(ValueError):
+            services.generate_totp(b'', True)
         # Check correct HMAC computation of custom implementation
         assert(custom_totp == pyhmac_totp)
+        # Check whether TOTP codes of correct length are generated
+        assert(len(str(custom_totp)) == 6)
         # Check if different TOTP codes are generated for different secrets
         assert(custom_totp != custom_totp2)
