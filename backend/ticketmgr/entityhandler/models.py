1b94e6c7c02a36fe741dd9a645ff1b0c9220445a
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-10-20 18:20:37 +0900

    Implement registration check methods

--- backend/ticketmgr/entityhandler/models.py
+++ backend/ticketmgr/entityhandler/models.py
@@ -19,11 +19,49 @@ class Event(models.Model):
     ev_datetime = models.DateTimeField()
     ev_hash = models.CharField(max_length = 16)
 
+    def user_is_registered(self, user: 'TktUser') -> bool:
+        """Checks if a user is registered for an event.
+
+        Args:
+            user (TktUser): The user being checked for registration.
+
+        Returns:
+            bool: True if user references this Event object.
+            False otherwise.
+        """
+        # Effectively equivalent to TktUser.registered_to_event()
+        # Django only caches forward lookups, so prefer whenever possible
+        return user.events.filter(pk=self.pk).exists()
+
+    def agent_is_registered(self, agent: 'TktAgent') -> bool:
+        """Checks if an agent is assigned to an event.
+
+        Args:
+            agent (TktAgent): The agent being checked for assignment.
+
+        Returns:
+            bool: True if agent references this Event object.
+            False otherwise.
+        """
+        return agent.event.pk == self.pk
+
 class TktUser(User):
     """Django User that represents your typical user.
     A user cannot create, modify, and register for events.
     """
     events = models.ManyToManyField(Event)
+
+    def registered_to_event(self, event: Event) -> bool:
+        """Checks if an event contains a user in its roster.
+
+        Args:
+            event (Event): The event of which roster is being checked.
+
+        Returns:
+            bool: True if this TktUser object references event.
+            False otherwise.
+        """
+        return self.events.filter(pk=event.pk).exists()
 
 class TktAdmin(User):
     """Django User that represents event administrators.
@@ -32,6 +70,8 @@ class TktAdmin(User):
     events = models.ManyToManyField(Event)
 
 class TktAgent(models.Model):
+    # TODO: check and enforce that an agent always gets assigned an event
+    # upon init
     """Django User that represents event agents.
     An agent can verify user tickets for events.
     """
