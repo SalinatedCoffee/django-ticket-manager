b62c1f6c4b3f3f66cb1ba6d00804db1cf9e651f5
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-10-21 17:11:11 +0900

    Use self.assert* and implement unit tests for services.generate_ticket_secret()

--- backend/ticketmgr/otphandler/tests.py
+++ backend/ticketmgr/otphandler/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.utils import timezone
+from entityhandler.models import Event, TktUser
 from otphandler import services
+
 
 class ServicesTestCase(TestCase):
     def test_totp_generation(self):
@@ -13,8 +16,33 @@ class ServicesTestCase(TestCase):
         with self.assertRaises(ValueError):
             services.generate_totp(b'', True)
         # Check correct HMAC computation of custom implementation
-        assert(custom_totp == pyhmac_totp)
+        self.assertEquals(custom_totp, pyhmac_totp)
         # Check whether TOTP codes of correct length are generated
-        assert(len(str(custom_totp)) == 6)
+        self.assertEquals(len(str(custom_totp)), 6)
         # Check if different TOTP codes are generated for different secrets
-        assert(custom_totp != custom_totp2)
+        self.assertNotEquals(custom_totp, custom_totp2)
+    
+    def test_ticket_secret_generation(self):
+        ev = Event.objects.create(ev_title="Some Event",
+                                  ev_description="Some event description",
+                                  ev_datetime=timezone.datetime.now(timezone.utc),
+                                  ev_hash='1234ABCD5678EFGH')
+        ev_shallow = Event(ev_title="Title", ev_description="Description",
+                           ev_datetime=timezone.datetime.now(timezone.utc),
+                           ev_hash='1234123412341234')
+        user = TktUser.objects.create_user('user', 'user@domain.com', 'userpassword')
+        user_shallow = TktUser('username', 'email', 'password')
+        user_shallow.is_active = False
+
+        with self.assertRaises(ValueError):
+            services.generate_ticket_secret(user, ev_shallow)
+        with self.assertRaises(ValueError):
+            services.generate_ticket_secret(user_shallow, ev)
+        with self.assertRaises(ValueError):
+            services.generate_ticket_secret(user_shallow, ev_shallow)
+
+        tkt_secret = services.generate_ticket_secret(user, ev)
+        
+        self.assertEqual(len(tkt_secret), 20)
+        self.assertEqual(tkt_secret[:10], b'CD5678EFGH')
+        self.assertEqual(tkt_secret[-10:], bytes(user.password[-10:], encoding='utf-8'))
