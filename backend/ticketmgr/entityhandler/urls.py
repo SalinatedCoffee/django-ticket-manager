fc603e6026c39e98f1e40b23052bc229d5b5fab4
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-03 18:58:59 +0900

    Finalize URI path configuration for entityhandler

--- backend/ticketmgr/entityhandler/urls.py
+++ backend/ticketmgr/entityhandler/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('signup', views.user_signup, name='user_signup'),
+    path('user', views.user_signup, name='user_signup'),
     path('user/<str:username>', views.user_info, name='user_info'),
     path('user/<str:username>/events', views.user_events, name='user_events'),
-    path('event/new', views.event_new, name='event_new'),
-    path('event/list', views.event_query, name='event_query'),
+    path('event/', views.events, name='events'),
     path('event/<int:event_id>', views.event_info, name='event_info'),
     path('event/<int:event_id>/users', views.event_users, name='event_users'),
     path('event/<int:event_id>/agents', views.event_agents, name='event_agents'),
