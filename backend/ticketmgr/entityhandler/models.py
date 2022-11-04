2df11cd44ee1b7a6bb3edc1aeb8060d3f0d37f13
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-04 16:18:19 +0900

    Use UUIDv4 for Event and TktUser and update docs accordingly

--- backend/ticketmgr/entityhandler/models.py
+++ backend/ticketmgr/entityhandler/models.py
@@ -1,6 +1,6 @@
 """Module that contains Django Models representing events and users.
 """
-
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,25 +8,25 @@ class Event(models.Model):
     """Django Model that represents an event.
 
     Attributes:
-        ev_title: A TextField() that contains the event name.
-        ev_description: A TextField() that contains the event description.
-        ev_date: A DateTimefield() that contains the starting date and time of the event.
-        ev_hash: A CharField() that contains a 128bit hash unique to the event.
+        ``title``: A ``TextField`` that contains the event name.
+        ``description``: A ``TextField`` that contains the event description.
+        ``date``: A ``DateTimefield`` that contains the starting date and time of the event.
+        ``uuid``: A ``UUIDField`` that contains the v4 UUID of the event.
     """
-    ev_title = models.TextField()
-    ev_description = models.TextField()
-    ev_datetime = models.DateTimeField()
-    ev_hash = models.CharField(max_length = 16)
+    title = models.TextField()
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def user_is_registered(self, user: 'TktUser') -> bool:
         """Checks if a user is registered for an event.
 
         Args:
-            user (TktUser): The user being checked for registration.
+            ``user`` (``TktUser``): The user being checked for registration.
 
         Returns:
-            bool: True if user references this Event object.
-            False otherwise.
+            ``bool``: ``True`` if user references this ``Event`` object.
+            ``False`` otherwise.
         """
         # Effectively equivalent to TktUser.registered_to_event()
         # Django only caches forward lookups, so prefer whenever possible
@@ -36,11 +36,11 @@ class Event(models.Model):
         """Checks if an agent is assigned to an event.
 
         Args:
-            agent (TktAgent): The agent being checked for assignment.
+            ``agent`` (``TktAgent``): The agent being checked for assignment.
 
         Returns:
-            bool: True if agent references this Event object.
-            False otherwise.
+            ``bool``: ``True`` if agent references this ``Event`` object.
+            ``False`` otherwise.
         """
         return agent.event.pk == self.pk
 
@@ -50,16 +50,17 @@ class TktUser(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     events = models.ManyToManyField(Event, blank=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def registered_to_event(self, event: Event) -> bool:
         """Checks if an event contains a user in its roster.
 
         Args:
-            event (Event): The event of which roster is being checked.
+            ``event`` (``Event``): The event of which roster is being checked.
 
         Returns:
-            bool: True if this TktUser object references event.
-            False otherwise.
+            ``bool``: ``True`` if this ``TktUser`` object references event.
+            ``False`` otherwise.
         """
         return self.events.filter(pk=event.pk).exists()
 
