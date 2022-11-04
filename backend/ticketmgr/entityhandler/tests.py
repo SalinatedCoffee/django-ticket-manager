30597492e1d4dcd31adad2cb67cac4efe2341f24
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-04 16:17:29 +0900

    Update tests to model refactoring specs

--- backend/ticketmgr/entityhandler/tests.py
+++ backend/ticketmgr/entityhandler/tests.py
@@ -3,56 +3,50 @@ from django.utils import timezone
 from .models import *
 
 TEST_EV_DATETIME = timezone.datetime.now(timezone.utc)
-TEST_EV_HASH = 'ABCD1234ABCD1234'   
-
 
 class EventsTestCase(TestCase):
     def setUp(self):
-        Event.objects.create(ev_title="Event 1",
-                             ev_description="This is the first event.",
-                             ev_datetime=TEST_EV_DATETIME,
-                             ev_hash=TEST_EV_HASH)
-        Event.objects.create(ev_title="Event 2",
-                             ev_description="This is the second event.",
-                             ev_datetime=timezone.datetime.now(timezone.utc),
-                             ev_hash=TEST_EV_HASH)
-        Event.objects.create(ev_title="Event 3",
-                             ev_description="This is the third event.",
-                             ev_datetime=timezone.datetime(9876, 5, 4,
-                                                           3, 2, 1,
-                                                           tzinfo=timezone.utc),
-                             ev_hash=TEST_EV_HASH)
+        Event.objects.create(title="Event 1",
+                             description="This is the first event.",
+                             datetime=TEST_EV_DATETIME)
+        Event.objects.create(title="Event 2",
+                             description="This is the second event.",
+                             datetime=timezone.datetime.now(timezone.utc))
+        Event.objects.create(title="Event 3",
+                             description="This is the third event.",
+                             datetime=timezone.datetime(9876, 5, 4,
+                                                        3, 2, 1,
+                                                        tzinfo=timezone.utc))
 
     def test_events_attrib_access(self):
-        ev1 = Event.objects.get(ev_title="Event 1")
-        ev2 = Event.objects.get(ev_title="Event 2")
-        ev3 = Event.objects.get(ev_title="Event 3")
+        ev1 = Event.objects.get(title="Event 1")
+        ev2 = Event.objects.get(title="Event 2")
+        ev3 = Event.objects.get(title="Event 3")
 
         # Test attribute access
-        self.assertEqual(ev1.ev_title, "Event 1")
-        self.assertEqual(ev1.ev_description, "This is the first event.")
-        self.assertEqual(ev1.ev_datetime, TEST_EV_DATETIME)
-        self.assertEqual(ev1.ev_hash, TEST_EV_HASH)
+        self.assertEqual(ev1.title, "Event 1")
+        self.assertEqual(ev1.description, "This is the first event.")
+        self.assertEqual(ev1.datetime, TEST_EV_DATETIME)
+        self.assertIsNotNone(ev1.uuid)
 
         # Test attribute access on different object
-        self.assertEqual(ev3.ev_title, "Event 3")
+        self.assertEqual(ev3.title, "Event 3")
         # Test correct DateTimeField behavior
-        self.assertEqual(ev3.ev_datetime, 
+        self.assertEqual(ev3.datetime, 
                          timezone.datetime(9876, 5, 4,
                                            3, 2, 1,
                                            tzinfo=timezone.utc))
 
         # Test invalid lookup query
         with self.assertRaises(Event.DoesNotExist):
-            Event.objects.get(ev_title="Some other event")
+            Event.objects.get(title="Some other event")
 
 
 class TktEntityTestCase(TestCase):
     def setUp(self):
-        ev = Event.objects.create(ev_title="Some Event",
-                                  ev_description="Dummy event instance for testing",
-                                  ev_datetime=TEST_EV_DATETIME,
-                                  ev_hash=TEST_EV_HASH)
+        ev = Event.objects.create(title="Some Event",
+                                  description="Dummy event instance for testing",
+                                  datetime=TEST_EV_DATETIME)
         TktUser.objects.create(user=User.objects.create_user('username',
                                     'user@somedomain.com',
                                     'userpassword'))
@@ -77,7 +71,7 @@ class TktEntityTestCase(TestCase):
 
     def test_agent_entity_model(self):
         agent = User.objects.get(username='agentname').tktagent
-        event = Event.objects.get(ev_title="Some Event")
+        event = Event.objects.get(title="Some Event")
 
         self.assertEqual(agent.agent.email, 'agent@somedomain.com')
         self.assertEqual(agent.event, event)
@@ -89,10 +83,9 @@ class ModelRelationshipTestCase(TestCase):
         users = []
         admins = []
         for i in range(1, 4):
-            evs.append(Event.objects.create(ev_title=f"Event {i}",
-                                            ev_description=f"This is event number {i}.",
-                                            ev_datetime=TEST_EV_DATETIME,
-                                            ev_hash=TEST_EV_HASH))
+            evs.append(Event.objects.create(title=f"Event {i}",
+                                            description=f"This is event number {i}.",
+                                            datetime=TEST_EV_DATETIME))
             users.append(TktUser.objects.create(user=User.objects.create_user(f'user{i}',
                                                      f'user{i}@domain.com',
                                                      f'user{i}password')))
@@ -176,16 +169,15 @@ class CheckRegistrationTestCase(TestCase):
         users = []
         agents = []
         for i in range(1, 3):
-            Event.objects.create(ev_title=f"Event {i}",
-                                 ev_description=f"This is event number {i}.",
-                                 ev_datetime=TEST_EV_DATETIME,
-                                 ev_hash=TEST_EV_HASH)
+            Event.objects.create(title=f"Event {i}",
+                                 description=f"This is event number {i}.",
+                                 datetime=TEST_EV_DATETIME)
             users.append(
                 TktUser.objects.create(user=User.objects.create_user(f'user{i}',
                                             f'user{i}@domain.com',
                                             f'password{i}')))
-        evs.append(Event.objects.get(ev_title="Event 1"))
-        evs.append(Event.objects.get(ev_title="Event 2"))
+        evs.append(Event.objects.get(title="Event 1"))
+        evs.append(Event.objects.get(title="Event 2"))
         for i in range(1, 3):
             user = User.objects.create_user(f'agent{i}',
                                             f'agent{i}@domain.com',
@@ -195,8 +187,8 @@ class CheckRegistrationTestCase(TestCase):
         users[1].events.add(evs[0])
 
     def test_event_methods(self):
-        ev1 = Event.objects.get(ev_title="Event 1")
-        ev2 = Event.objects.get(ev_title="Event 2")
+        ev1 = Event.objects.get(title="Event 1")
+        ev2 = Event.objects.get(title="Event 2")
         user1 = User.objects.get(username='user1').tktuser
         user2 = User.objects.get(username='user2').tktuser
         agent1 = User.objects.get(username='agent1').tktagent
@@ -210,8 +202,8 @@ class CheckRegistrationTestCase(TestCase):
         self.assertFalse(user2.registered_to_event(ev2))
 
     def test_tktuser_methods(self):
-        ev1 = Event.objects.get(ev_title="Event 1")
-        ev2 = Event.objects.get(ev_title="Event 2")
+        ev1 = Event.objects.get(title="Event 1")
+        ev2 = Event.objects.get(title="Event 2")
         user1 = User.objects.get(username='user1').tktuser
         user2 = User.objects.get(username='user2').tktuser
 
