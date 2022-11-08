90ddc47ba545fd34db73ace21fba0eb8a2042113
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-08 13:22:32 +0900

    Make formatting prettier and reference Events by uuid

--- backend/ticketmgr/entityhandler/views.py
+++ backend/ticketmgr/entityhandler/views.py
@@ -11,12 +11,10 @@ def user_signup(request):
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
-        user_raw = serializer.save()
-        tktuser_raw = TktUser.objects.create(user=user_raw)
-
-        return Response(TktUserSerializer(tktuser_raw).data,
+        user = serializer.save()
+        tktuser = TktUser.objects.create(user=user)
+        return Response(TktUserSerializer(tktuser).data,
                         status.HTTP_201_CREATED)
-
     return Response(serializer.errors, status.HTTP_409_CONFLICT)
 
 @api_view(['GET'])
@@ -29,43 +27,44 @@ def user_info(request, username):
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = TktUserSerializer(user)
-
         return Response(serializer.data, status.HTTP_200_OK)
 
 @api_view(['GET', 'POST'])
 def user_events(request, username):
-    """``GET``: Returns a JSON representation of all ``Event`` objects referenced by
-    a ``TktUser`` with ``username``.
-    ``POST``: Adds a reference to an ``Event`` if it exists.
+    """First queries a ``TktUser`` with ``username``. Then on
+    ``GET``: Returns a JSON representation of all ``Event`` objects referenced by
+    the queried ``TktUser``.
+    ``POST``: Adds a reference to an ``Event`` with ``event_uuid``
+    to the queried ``TktUser``.
+    JSON format: ``{'event_uuid': <str:uuid>}``
     """
+    try:
+        user = User.objects.get(username=username).tktuser
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
-        try:
-            user = User.objects.get(username=username).tktuser
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        events = EventSerializer(list(user.events.all()), many=True)
-        return Response(events.data)
+        # TODO: Define separate serializers for list/detail view
+        serializer = EventSerializer(list(user.events.all()), many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
     
     elif request.method == 'POST':
         try:
-            user = User.objects.get(username=username).tktuser
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        try:
-            event = Event.objects.get(pk=request.data['ev_id'])
+            event = Event.objects.get(uuid=request.data['event_uuid'])
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         user.events.add(event)
         return Response(EventSerializer(event).data)
 
 @api_view(['GET', 'POST'])
-def events(request):
+def event(request):
     """``GET``: Returns a JSON representation of all ``Event`` objects.
     ``POST``: Creates a new ``Event``.
     """
     if request.method == 'GET':
-        events = EventSerializer(list(Event.objects.all()), many=True)
-        return Response(events.data)
+        serializer = EventSerializer(list(Event.objects.all()), many=True)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
         serial = EventSerializer(data=request.data)
         if serial.is_valid():
@@ -74,60 +73,63 @@ def events(request):
         return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def event_info(request, event_id):
-    """Returns a JSON representation of an ``Event`` with ``event_id``.
+def event_info(request, event_uuid):
+    """Returns a JSON representation of an ``Event`` with ``event_uuid``.
     """
     if request.method == 'GET':
         try:
-            event = EventSerializer(Event.objects.get(pk=event_id))
+            event = EventSerializer(Event.objects.get(uuid=event_uuid))
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        return Response(event.data)
+        return Response(event.data, status.HTTP_200_OK)
 
 @api_view(['GET', 'POST'])
-def event_users(request, event_id):
+def event_users(request, event_uuid):
     """``GET``: Returns a JSON representation of all ``TktUser``s that reference
-    ``Event`` with ``event_id``.
+    ``Event`` with ``event_uuid``.
     ``POST``: Adds a reference to ``Event`` if it exists.
     """
     if request.method == 'GET':
         try:
-            event = Event.objects.get(pk=event_id)
+            event = Event.objects.get(uuid=event_uuid)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         users = TktUserSerializer(event.tktuser_set.all(), many=True)
         return Response(users.data)
+
     elif request.methods == 'POST':
         return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 @api_view(['GET', 'POST'])
-def event_agents(request, event_id):
+def event_agents(request, event_uuid):
     """``GET``: Returns a JSON representation of all ``TktAgent``s that reference
     ``Event`` with ``event_id``.
     ``POST``: Adds a reference to ``Event`` if it exists.
     """
     if request.method == 'GET':
         try:
-            event = Event.objects.get(pk=event_id)
+            event = Event.objects.get(uuid=event_uuid)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         agents = TktAgentSerializer(event.tktagent_set.all(), many=True)
         return Response(agents.data)
+
     elif request.method == 'POST':
         return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 @api_view(['GET', 'POST'])
-def event_admins(request, event_id):
+def event_admins(request, event_uuid):
     """``GET``: Returns a JSON representation of all ``TktAdmin``s that reference
     ``Event`` with ``event_id``.
     ``POST``: Adds a reference to ``Event`` if it exists.
     """
     if request.method == 'GET':
         try:
-            event = Event.objects.get(pk=event_id)
+            event = Event.objects.get(uuid=event_uuid)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         admins = TktAdminSerializer(event.tktadmin_set.all(), many=True)
         return Response(admins.data)
+
     elif request.methods == 'POST':
         return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
