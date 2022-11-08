b7a3b2c7cb4b0c84556697bef5068d4121c1c6fc
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-08 13:21:38 +0900

    Use singular nouns in URIs

--- backend/ticketmgr/entityhandler/urls.py
+++ backend/ticketmgr/entityhandler/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 urlpatterns = [
     path('user', views.user_signup, name='user_signup'),
     path('user/<str:username>', views.user_info, name='user_info'),
-    path('user/<str:username>/events', views.user_events, name='user_events'),
-    path('event/', views.events, name='events'),
+    path('user/<str:username>/event', views.user_events, name='user_events'),
+    path('event/', views.event, name='event'),
     path('event/<int:event_id>', views.event_info, name='event_info'),
-    path('event/<int:event_id>/users', views.event_users, name='event_users'),
-    path('event/<int:event_id>/agents', views.event_agents, name='event_agents'),
-    path('event/<int:event_id>/admins', views.event_admins, name='event_admins')
+    path('event/<int:event_id>/user', views.event_users, name='event_users'),
+    path('event/<int:event_id>/agent', views.event_agents, name='event_agents'),
+    path('event/<int:event_id>/admin', views.event_admins, name='event_admins')
 ]
