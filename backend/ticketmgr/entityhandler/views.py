b728709749118d2918026ece353f3aeff2af036c
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-08 12:56:22 +0900

    Commit initial implementation of user_signup and add docstrings

--- backend/ticketmgr/entityhandler/views.py
+++ backend/ticketmgr/entityhandler/views.py
@@ -1,6 +1,4 @@
-from . import models
 from .serializers import *
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,21 +6,38 @@ from rest_framework.response import Response
 
 @api_view(['POST'])
 def user_signup(request):
-    return Response({'user_signup': 'none'})
+    """Creates a new ``User`` which is then linked to a new ``TktUser``.
+    """
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user_raw = serializer.save()
+        tktuser_raw = TktUser.objects.create(user=user_raw)
+
+        return Response(TktUserSerializer(tktuser_raw).data,
+                        status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status.HTTP_409_CONFLICT)
 
 @api_view(['GET'])
 def user_info(request, username):
+    """Returns a JSON representation of a ``TktUser`` with ``username``.
+    """
     if request.method == 'GET':
         try:
             user = User.objects.get(username=username).tktuser
-            print(type(user))
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serial = TktUserSerializer(user)
-        return Response(serial.data)
+        serializer = TktUserSerializer(user)
+
+        return Response(serializer.data, status.HTTP_200_OK)
 
 @api_view(['GET', 'POST'])
 def user_events(request, username):
+    """``GET``: Returns a JSON representation of all ``Event`` objects referenced by
+    a ``TktUser`` with ``username``.
+    ``POST``: Adds a reference to an ``Event`` if it exists.
+    """
     if request.method == 'GET':
         try:
             user = User.objects.get(username=username).tktuser
@@ -30,6 +45,7 @@ def user_events(request, username):
             return Response(status=status.HTTP_404_NOT_FOUND)
         events = EventSerializer(list(user.events.all()), many=True)
         return Response(events.data)
+    
     elif request.method == 'POST':
         try:
             user = User.objects.get(username=username).tktuser
@@ -44,6 +60,9 @@ def user_events(request, username):
 
 @api_view(['GET', 'POST'])
 def events(request):
+    """``GET``: Returns a JSON representation of all ``Event`` objects.
+    ``POST``: Creates a new ``Event``.
+    """
     if request.method == 'GET':
         events = EventSerializer(list(Event.objects.all()), many=True)
         return Response(events.data)
@@ -56,6 +75,8 @@ def events(request):
 
 @api_view(['GET'])
 def event_info(request, event_id):
+    """Returns a JSON representation of an ``Event`` with ``event_id``.
+    """
     if request.method == 'GET':
         try:
             event = EventSerializer(Event.objects.get(pk=event_id))
@@ -65,6 +86,10 @@ def event_info(request, event_id):
 
 @api_view(['GET', 'POST'])
 def event_users(request, event_id):
+    """``GET``: Returns a JSON representation of all ``TktUser``s that reference
+    ``Event`` with ``event_id``.
+    ``POST``: Adds a reference to ``Event`` if it exists.
+    """
     if request.method == 'GET':
         try:
             event = Event.objects.get(pk=event_id)
@@ -73,10 +98,14 @@ def event_users(request, event_id):
         users = TktUserSerializer(event.tktuser_set.all(), many=True)
         return Response(users.data)
     elif request.methods == 'POST':
-        return Response({'event_users': 'none'})
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 @api_view(['GET', 'POST'])
 def event_agents(request, event_id):
+    """``GET``: Returns a JSON representation of all ``TktAgent``s that reference
+    ``Event`` with ``event_id``.
+    ``POST``: Adds a reference to ``Event`` if it exists.
+    """
     if request.method == 'GET':
         try:
             event = Event.objects.get(pk=event_id)
@@ -89,6 +118,10 @@ def event_agents(request, event_id):
 
 @api_view(['GET', 'POST'])
 def event_admins(request, event_id):
+    """``GET``: Returns a JSON representation of all ``TktAdmin``s that reference
+    ``Event`` with ``event_id``.
+    ``POST``: Adds a reference to ``Event`` if it exists.
+    """
     if request.method == 'GET':
         try:
             event = Event.objects.get(pk=event_id)
