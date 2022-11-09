0b88f57ab6ab285718d648fc2ea33096f47f0be7
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-09 16:02:12 +0900

    Commit initial implementation of ticket_auth()

--- backend/ticketmgr/otphandler/views.py
+++ backend/ticketmgr/otphandler/views.py
@@ -8,8 +8,9 @@ from rest_framework.response import Response
 @api_view(['POST'])
 def ticket_new(request):
     """Adds a reference to an ``Event`` with ``event_uuid`` to a ``TktUser``
-    with ``event_uuid``.
-    On success, sends a JSON payload with the ticket-unique secret.
+    with ``user_uuid``.
+    On success, sends a JSON response with the ticket-unique secret.
+    JSON format: {'user_uuid': <str>, 'event_uuid': <str>}
     """
     if request.header == 'POST':
         try:
@@ -34,5 +35,36 @@ def ticket_new(request):
 
 @api_view(['POST'])
 def ticket_auth(request):
+    """Verifies a ticket-generated TOTP code given an ``Event`` and ``TktUser``.
+    JSON format: {'user_uuid': <str>, 'event_uuid': <str>, 'ticket_totp': <str>}
+    """
+    if request.method == 'POST':
+        try:
+            user = TktUser.objects.get(uuid=request.data['user_uuid'])
+        except:
+            return Response({'error': 'User does not exist.'},
+                            status.HTTP_404_NOT_FOUND)
+        
+        try:
+            event = Event.objects.get(uuid=request.data['event_uuid'])
+        except:
+            return Response({'error': 'Event does not exist.'},
+                            status.HTTP_404_NOT_FOUND)
+        
+        try:
+            totp = request.data['ticket_totp']
+        except:
+            return Response({'error': 'TOTP code was not supplied.'},
+                            status.HTTP_400_BAD_REQUEST)
 
-    return Response({'ticket_auth': 'none'})
+        if not event.user_is_registered(user):
+            return Response({'error': 'User is not registered to event.'},
+                            status.HTTP_400_BAD_REQUEST)
+        tkt_secret = services.generate_ticket_secret(user, event)
+        totp_authority = str(services.generate_totp(tkt_secret))
+        if totp == totp_authority:
+            return Response({'ticket_is_valid': True},
+                            status.HTTP_200_OK)
+        else:
+            return Response({'ticket_is_valid': False},
+                            status.HTTP_200_OK)
