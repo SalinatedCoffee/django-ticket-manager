c0aaf191fac5c31c4eacc451f8d1d6324b2513e9
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-11-09 15:44:48 +0900

    Commit initial implementation of ticket_new()

--- backend/ticketmgr/otphandler/views.py
+++ backend/ticketmgr/otphandler/views.py
@@ -1,9 +1,33 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+import services
+from entityhandler.models import *
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
-def ticket_new(response):
-    return JsonResponse({'ticket_new': 'none'})
 
-def ticket_auth(response):
-    return JsonResponse({'ticket_auth': 'none'})
+@api_view(['POST'])
+def ticket_new(request):
+    try:
+        user = TktUser.objects.get(uuid=request.data['user_uuid'])
+    except:
+        return Response({'error': 'User does not exist.'},
+                        status.HTTP_404_NOT_FOUND)
+    
+    try:
+        event = Event.objects.get(uuid=request.data['event_uuid'])
+    except:
+        return Response({'error': 'Event does not exist.'},
+                        status.HTTP_404_NOT_FOUND)
+
+    if request.header == 'POST':
+        if event.user_is_registered(user):
+            return Response({'error': 'User already registered to event.'},
+                            status.HTTP_409_CONFLICT)
+        user.events.add(event)
+        tkt_secret = services.generate_ticket_secret(user, event)
+        return Response({'success': tkt_secret},
+                        status.HTTP_200_OK)
+
+@api_view(['POST'])
+def ticket_auth(request):
+    return Response({'ticket_auth': 'none'})
