e2e7fcb1c308bc90cb84d91b004bbd14ea972de6
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-12-16 17:33:33 +0900

    Add stubs for service functions

---
+++ frontend/ticketmgr/src/services.js
@@ -0,0 +1,51 @@
+// Backend interaction
+
+function register() {
+    return
+}
+
+function login() {
+    return
+}
+
+function logout() {
+    return
+}
+
+function entityDetails() {
+    return
+}
+
+function eventDetails() {
+    return
+}
+
+function eventList() {
+    return
+}
+
+function eventRegisteredEntityList() {
+    return
+}
+
+function createNewEvent() {
+    return
+}
+
+// Local operations
+
+function getNewCsrf() {
+    return
+}
+
+function refreshSession() {
+    return
+}
+
+function saveTotpSecret() {
+    return
+}
+
+function getTotpSecret() {
+    return
+}
