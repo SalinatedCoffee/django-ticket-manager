5fb2c1f012461a8522cb278caa3b1d5d9374f526
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-12-19 14:36:49 +0900

    Add terse comments for temporary use

--- frontend/ticketmgr/src/services.js
+++ frontend/ticketmgr/src/services.js
@@ -1,51 +1,78 @@
 // Backend interaction
 
-function register() {
+const ENDPOINT_URI = "http://localhost:8000/api/"
+
+export const TktEntType = {
+    // enum for usertypes
+    User: Symbol("tktuser"),
+    Agent: Symbol("tktagent"),
+    Admin: Symbol("tktadmin")
+};
+
+export async function register(info) {
+    // this should create a new tktuser account given json as input
+    // return true and valid json obj on success
+    // other entity types are currently unsupported by endpoint
+    fetch(ENDPOINT_URI+"user", JSON.stringify(info))
+      .then(response => {
+        if (response.status !== 200) return (false, response.body.json())
+        else return (true, response.body.json())
+      })
+}
+
+export async function login() {
+    // this should log a user in given json as input
+    // return true on success
     return
 }
 
-function login() {
+export async function logout() {
+    // this should log a user out
+    // return true on success
     return
 }
 
-function logout() {
+export async function entityDetails() {
+    // this should fetch user details given username
+    // return valid json on success with tkt-entity type embedded
     return
 }
 
-function entityDetails() {
+export async function eventDetails() {
+    // this should fetch event details given uuid
+    // return valid json on success (including uuid)
     return
 }
 
-function eventDetails() {
+export async function eventList() {
+    // this should fetch list of all events
+    // return valid json on success
+    // {ev details}, {ev details}, ...
     return
 }
 
-function eventList() {
+export async function eventRegisteredEntityList() {
+    // this should fetch list of all entities enrolled to event given uuid
+    // return valid json on success
     return
 }
 
-function eventRegisteredEntityList() {
-    return
-}
-
-function createNewEvent() {
+export async function createNewEvent() {
+    // this should create new event given json
+    // return true, valid json on success
     return
 }
 
 // Local operations
 
-function getNewCsrf() {
+export function saveTotpSecret() {
+    // this should save totp to safe local storage given event uuid
+    // return true on success
     return
 }
 
-function refreshSession() {
-    return
-}
-
-function saveTotpSecret() {
-    return
-}
-
-function getTotpSecret() {
+export function getTotpSecret() {
+    // this should retrieve totp from local storage
+    // return totp on success, false otherwise
     return
 }
